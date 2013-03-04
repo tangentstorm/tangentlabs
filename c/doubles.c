59a78ceae00f6b45b6e1855ef1d125556747953d
tangentstorm <michal.wallace@gmail.com>
2013-03-03 23:46:35 -0600

    doubles.c: sprintf the number, then sscanf it back in

--- c/doubles.c
+++ c/doubles.c
@@ -1,14 +1,19 @@
 // You should never really use doubles for currency, but
-// if somebody insisted you had to, how bad would it be?
+// if somebody insisted you had to, (a professor, say),
+// how bad would it be?
 //
 // gcc 4.7.prints the following sequence without rounding
 // errors on my machine. (4.7.2-2ubuntu1)
 #include <stdio.h>
 
 void main() {
-  double i = 0.00;
+  double j, i;
+  char str[64];
+  i = 0.00;
   while (i < 1.0) {
-    printf("%f\n", i);
-    i += 0.01;
+    sprintf(str, "%f", i);    // render double as string
+    sscanf(str, "%lf", &j);   // parse it back in again
+    printf("%f %f\n", i, j);  // print both side by side
+    i += 0.001;
   }
 }
