81401d401c3ed5abe7d96b119de13685762e3e2f
tangentstorm <michal.wallace@gmail.com>
2013-04-21 08:32:58 -0500

    refcounts: added some names, and experement with var/const parameters.

--- pascal/refcounts.pas
+++ pascal/refcounts.pas
@@ -4,17 +4,24 @@ program refcounts;
   type
     IRef = interface
       procedure Boop;
-    end; 
-    TObj= class (IRef)
+    end;
+    TObj = class (IRef)
+      constructor Create( name : string );
       // these are the IUnknown methods to implement :
       function QueryInterface(constref guid : TGuid; out obj ):LongInt; cdecl;
       function _AddRef: longint; cdecl;
       function _Release: longint; cdecl;
       procedure Boop;
-    private					 
-      count						 : integer;
-    end;						 
-
+    private
+      _count : integer;
+      _name  : string;
+    end;
+    
+    constructor TObj.Create( name : string );
+    begin
+      _name := name
+    end;
+    
     function TObj.QueryInterface(constref guid : TGuid; out obj ):LongInt; cdecl;
     begin
       result := 0;
@@ -22,50 +29,98 @@ program refcounts;
 
     function TObj._AddRef : longint; cdecl;
     begin
-      inc( count );
-      WriteLn( 'AddRef: Count =', count );
-      result := count;
+      inc( _count );
+      WriteLn( 'AddRef: ', _name, '.count = ', _count );
+      result := 0;
     end;
 
     function TObj._Release : longint; cdecl;
     begin
-      dec( count );
-      WriteLn( 'Release: Count =', count );
-      result := count;
+      dec( _count );
+      WriteLn( 'Release: ', _name, '.count = ', _count );
+      result := 0;
     end;
 
     procedure TObj.Boop;
     begin
-      WriteLn( 'The computer says "boop". Can you say "boop"?' );
+      WriteLn( '<Boop>' );
     end;
 
     procedure DoSomething( whatever :  IRef );
     begin
-      writeln(': --> DoSomething');
+      writeln(': --> DoSomething (normal parameter)');
       whatever.boop;
+      writeln(':     reassigning local reference to new instance:');
+      whatever := TObj.Create( 'C' );
       writeln(': <-- DoSomething');
+    end;
+
+    procedure WithConst( const whatever :  IRef );
+    begin
+      writeln(': --> WithConst');
+      whatever.boop;
+      writeln(': <-- WithConst');
+    end;
+
+    procedure WithVar( var whatever :  IRef );
+    begin
+      writeln(': --> WithVar');
+      whatever.boop;
+      writeln(':     reassigning local reference to new instance:');
+      whatever := TObj.Create( 'B' );
+      writeln(': <-- WithVar');
     end;
 
 var ref : IRef;
 begin
   writeln(': --> main');
-  writeln(':     Calling Constructor.');
-  ref := TObj.Create;
+  writeln(':     Constructing instance.');
+  ref := TObj.Create( 'A' );
+
   writeln(':     Calling DoSomething.');
   DoSomething(ref);
+  writeln(':     Back.');
+
+  writeln(':     Calling WithVar.');
+  WithVar(ref);
+  writeln(':     Back.');
+
+  writeln(':     Calling WithConst.');
+  WithConst(ref);
+  writeln(':     Back.');
+
   writeln(': <-- main');
 end.
-  
+
 { output:
-    : --> main
-    :     Calling Constructor.
-    AddRef: Count =1
-    :     Calling DoSomething.
-    AddRef: Count =2
-    : --> DoSomething
-    The computer says "boop". Can you say "boop"?
-    : <-- DoSomething
-    Release: Count =1
-    : <-- main
-    Release: Count =0
+
+  : --> main
+  :     Constructing instance.
+  AddRef: A.count = 1
+  :     Calling DoSomething.
+  AddRef: A.count = 2
+  : --> DoSomething (normal parameter)
+  <Boop>
+  :     reassigning local reference to new instance:
+  AddRef: C.count = 1
+  Release: A.count = 1
+  : <-- DoSomething
+  Release: C.count = 0
+  :     Back.
+  :     Calling WithVar.
+  : --> WithVar
+  <Boop>
+  :     reassigning local reference to new instance:
+  AddRef: B.count = 1
+  Release: A.count = 0
+  : <-- WithVar
+  :     Back.
+  :     Calling WithConst.
+  : --> WithConst
+  <Boop>
+  : <-- WithConst
+  :     Back.
+  : <-- main
+  Release: B.count = 0
+
 }
