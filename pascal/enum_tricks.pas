f9acfb84a74d135996883556ac638242d57d2e09
tangentstorm <michal.wallace@gmail.com>
2013-05-17 13:10:10 -0500

    cleaned up enum_tricks for article about sets

--- pascal/enum_tricks.pas
+++ pascal/enum_tricks.pas
@@ -9,32 +9,54 @@ const
 procedure writedays( title : string; whichdays : TDays );
   var d : Day;
   begin
-    writeln( title );
-    for d in whichdays do write( '  ', d );
-    writeln;
+    write( title, ' [' );
+    for d in whichdays do write( ' ', d );
+    writeln( ' ]');
   end;
 
-var mwf : TDays = [ mon, wed, fri ];
+  var mwf : TDays = [ mon, wed, fri ];
+  var d,w,s  : set of day;
 begin
-  writedays( 'all days:', [low(day) .. high(day)]);
-  writedays( 'weekdays:', weekdays );
-  writedays( 'weekend:',  [ sat, sun ]);
-  writedays( 'MWF:', mwf  );
-  writedays( 'MWF + weekend :', mwf + [ sat, sun ]);
-  writedays( 'weekdays but not MWF', weekdays - mwf );
+
+  w := weekdays;              // weekdays
+  s := mwf + [sat, sun];      // arbitrary set of days
+  writedays( 's:', s);
+  writedays( 'w:', w);
+  writeln;
+  writedays( ' w + s:',  w + s ); // union
+  writedays( ' w * s:',  w * s ); // intersection
+  writedays( ' w - s:',  w - s ); // difference
+  writedays( ' s - w:',  s - w ); // difference
+  writedays( 'w >< s:', w><s );   // symmetric difference
+  writeln;
+  writeln( '  w   <= s: ', w <= s );    // subset?
+  writeln( '  mwf <= w: ', mwf <= w );  // subset?
+  writeln( '  w   >= s: ', w >= s );    // superset?
+  writeln( '  w    = s: ', w = s );     // equal
+  writeln( '  w   <> s: ', w <> s );    // not-equal
+  writeln( 'mon in mwf: ', mon in mwf); // element-of?
+  writeln( 'tue in mwf: ', mon in mwf); // element-of?
+  writeln;
+
 end.
 
 { output :
-  all days:
-    mon  tue  wed  thu  fri  sat  sun
-  weekdays:
-    mon  tue  wed  thu  fri
-  weekend:
-    sat  sun
-  MWF:
-    mon  wed  fri
-  MWF + weekend :
-    mon  wed  fri  sat  sun
-  weekdays but not MWF
-    tue  thu
+  
+  s: [ mon wed fri sat sun ]
+  w: [ mon tue wed thu fri ]
+
+  w + s: [ mon tue wed thu fri sat sun ]
+  w * s: [ mon wed fri ]
+  w - s: [ tue thu ]
+  s - w: [ sat sun ]
+  w >< s: [ tue thu sat sun ]
+
+  w   <= s: FALSE
+  mwf <= w: TRUE
+  w   >= s: FALSE
+  w    = s: FALSE
+  w   <> s: TRUE
+  mon in mwf: TRUE
+  tue in mwf: TRUE
+
 }
