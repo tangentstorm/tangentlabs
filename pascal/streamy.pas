775a3c690d1ead6a7dbcae26105f51a5a7bff790
tangentstorm <michal.wallace@gmail.com>
2013-06-16 02:02:23 -0500

    tried out the lazarus reader/writer

--- pascal/streamy.pas
+++ pascal/streamy.pas
@@ -1,5 +1,5 @@
 { just experimenting with streaming components }
-{$mode objfpc}
+{$mode objfpc}{$H+}
 program streamy;
 uses classes, lresources;
   
@@ -13,38 +13,61 @@ type
       property name : string read _name write _name;
     end;
 
+
+type
+  TLoadFactory = class
+    procedure LoadTStreamy(	      
+		      aReader	      : TReader;
+		const aClassname      : String;
+		  var aComponentClass : TComponentClass );
+  end;
+  procedure TLoadFactory.LoadTStreamy(
+		      aReader	      : TReader;
+		const aClassname      : String;
+		  var aComponentClass : TComponentClass );
+  begin
+    writeln('about to read:', aClassName );
+    aComponentClass := TStreamyComponent;
+  end; { just }
+    
+						    
 var
-  stream : TStringStream;
-  compy	 : TStreamyComponent;
-  name	 : string = '';
-  data	 : string = '';
+  LoadFActory	       : TLoadFactory;
+  OnFindComponentClass : TFindComponentClassEvent; 
+  stream	       : TStringStream;
+  before	       : TStreamyComponent;
+  after 	       : TComponent;
+  name		       : string = '';
+  data		       : string = '';
 begin
-  compy := TStreamyComponent.Create(nil);
+  OnFindComponentClass := @LoadFactory.LoadTStreamy;
+  before := TStreamyComponent.Create(nil);
   stream := TStringStream.Create(data);
 
   registerClass(TStreamyComponent);
   
   write('enter a name:');
-  readln(name); compy.name := name;
+  readln(name); before.name := name;
   writeln('okie doke. serializing:');
-  compy.id := 1;
-  stream.writecomponent(compy);
+  before.id := 1;
+  stream.writecomponent(before);
 
-  compy.id := 2;
-  stream.writecomponent(compy);
+  before.id := 2;
+  WriteComponentAsTextToStream(stream, before);
   writeln('freeing component');
-  compy.free;
+  before.free;
 
   writeln('data string:');
   writeln(stream.datastring);
   stream.seek(0, soFromBeginning);
   writeln('loading component from the stream');
 
-  compy := stream.ReadComponent(nil) as TStreamyComponent;
-  writeln('hello again, ', compy.name, '. id =', compy.id );
+  before := stream.ReadComponent(nil) as TStreamyComponent;
+  writeln('hello again, ', before.name, '. id =', before.id );
 
-  compy := stream.ReadComponent(nil) as TStreamyComponent;
-  writeln('hello again, ', compy.name, '. id =', compy.id );
+  ReadComponentFromTextStream(stream, after, OnFindComponentClass);
+  with after as TStreamyComponent do
+    writeln('hello again, ', name, '. id =', id );
 end.
 
   
