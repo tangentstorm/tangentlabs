2fb5c1d3f65f9c2db12ad837c07eb5fc6d6ae42b
tangentstorm <michal.wallace@gmail.com>
2013-06-17 14:13:23 -0500

    blogspider: initial version, generated by scrapy

---
+++ python/blogspider/blogspider/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for blogspider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'blogspider'
+
+SPIDER_MODULES = ['blogspider.spiders']
+NEWSPIDER_MODULE = 'blogspider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'blogspider (+http://www.yourdomain.com)'
