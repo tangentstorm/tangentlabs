f36d721e55578ff795f394df2fa4e25d337d7d8f
tangentstorm <michal.wallace@gmail.com>
2013-06-17 21:02:45 -0500

    match.pas: cleaned up the syntax a bit

--- pascal/match.pas
+++ pascal/match.pas
@@ -9,23 +9,14 @@ uses sysutils;
 var MatchClass : TClass;
 		   
 { macros to make the syntax work }
-  {$define match   := try MatchClass := }
-  {$define against := .ClassType; raise MatchClass.InitInstance(MatchClass.NewInstance) except }
-  {$define when	   := on}
+  {$define given  := try MatchClass := }
+  {$define match  := .ClassType; raise MatchClass.InitInstance(MatchClass.NewInstance) except }
+  {$define when   := on}
 
 type
   TAnimal     = class end;
   TVegetable  = class end;
   TMineral    = class end;
-
-procedure dispatch( obj : TObject );
-  begin
-    match obj against
-      when TAnimal do writeln('animal!');
-      when TVegetable do writeln('vegetable!');
-      when TMineral do writeln('mineral!');
-    end
-  end;
 
 var
   objects : array[0..2] of TObject;
@@ -35,10 +26,14 @@ begin
   objects[0] := TAnimal.Create;
   objects[1] := TVegetable.Create;
   objects[2] := TMineral.Create;
-
   for i := 1 to 10 do
-    dispatch( objects[ random(3) ]);
-
+    given
+      objects[ random(3) ]
+    match
+      when TAnimal    do writeln('animal!');
+      when TVegetable do writeln('vegetable!');
+      when TMineral   do writeln('mineral!');
+    end
 end.
 
 { example output:
