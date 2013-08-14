b99c935a7915f0f1e5b5dbbe71efbd37f97d972c
tangentstorm <michal.wallace@gmail.com>
2013-08-14 00:52:00 -0500

    c++ buffer overflows

---
+++ c++/overflow.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+     
+int main(){
+  char user_name[5]; char overflow[100];
+  std::cout << "What's your name? ";
+  std::cin >> user_name;
+  std::cout << overflow << std::endl;
+}
