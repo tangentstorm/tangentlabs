c2aad0bc4f55b9156934d884f4020d27415f17b4
tangentstorm <michal.wallace@gmail.com>
2013-08-22 20:03:36 -0500

    negativerange.pas

---
+++ pascal/negativerange.pas
@@ -0,0 +1,35 @@
+{$mode fpc}
+program negativerange;
+
+var
+  ints : array[-10..10] of integer;
+  i    : integer;
+begin
+  for i := -10 to 10 do ints[i] := i*i;
+  for i := 10 downto -10 do writeln(i:3, ':', ints[i]:4);
+end.
+
+{ output:
+
+  10: 100
+  9:  81
+  8:  64
+  7:  49
+  6:  36
+  5:  25
+  4:  16
+  3:   9
+  2:   4
+  1:   1
+  0:   0
+  -1:   1
+  -2:   4
+  -3:   9
+  -4:  16
+  -5:  25
+  -6:  36
+  -7:  49
+  -8:  64
+  -9:  81
+  -10: 100
+}
