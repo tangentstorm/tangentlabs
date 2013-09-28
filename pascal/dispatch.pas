a0c974d7d34c2760469a2a774153e4b1d69ef106
tangentstorm <michal.wallace@gmail.com>
2013-09-28 10:42:34 -0500

    further monster/tasty-pet experiments, now with helpers

--- pascal/dispatch.pas
+++ pascal/dispatch.pas
@@ -1,36 +1,124 @@
-{
-  Experiment to test whether free pascal could do
-  dynamic type dispatch on an argument.
-
-  This isn't possible with the case statement,
-  but you can manually check each possible type
-  with the 'is' operator. It's not pretty,
-  but it works.
-}
+{--------------------------------------------------------------
+  TastyPets : Experiments in type-based dispatch.
+ --------------------------------------------------------------}
 {$mode objfpc}
-program dispatch;
+program tastypets;
 
+{--------------------------------------------------------------
+  We begin by collecting a variety of small animals to
+  use in our scientific testing.
+ --------------------------------------------------------------}
 type
-  pet = class (TObject)
+  TPet = class (TObject)
     _name : string;
     property name:string read _name;
     constructor create( newName : string );
   end;
-  dog = class ( pet ) end;
-  cat = class ( pet ) end;
-  hamster = class ( pet ) end;
-  monster = class
-    procedure eat( p : pet );
-    procedure eat( d : dog );
-    procedure eat( c : cat );
-  end;
 
-constructor pet.create( newName : string );
+constructor TPet.create( newName : string );
   begin
     _name := newName;
   end;
 
-procedure monster.eat( p : pet );
+type
+  TPuppyDog = class ( TPet ) end;
+  TKittyCat = class ( TPet ) end;
+  THamster = class ( TPet ) end;
+  TFrog = class ( TPet ) end;
+
+{--------------------------------------------------------------
+  We then proceed to serve an example of each type
+  to various monsters, and carefully note their
+  reactions.
+ --------------------------------------------------------------}
+var dog, cat, hamster, frog : TPet;
+procedure PrepareBuffet;
+  begin
+    dog := TPuppyDog.Create('Sparky');
+    cat := TKittyCat.Create('Quaxo');
+    hamster := THamster.Create('Goldie');
+    frog := TFrog.Create('Mr Jumpy');
+  end;
+
+type
+  TReaction = ( kYUCK,  kYUM, kNOEAT, kRAAR );
+  TMonster = class
+    function Taste( p : TPet ) : TReaction; virtual; abstract;
+    function TasteTest : boolean; virtual; abstract;
+  end;
+
+{--------------------------------------------------------------
+  The goal of our research is to find a monster
+  that reacts to pets in the following ways:
+--------------------------------------------------------------}
+type
+  TGoalMonster = class (TMonster)
+    function Taste( p : TPet ) : TReaction;
+      override; // we need this for TMonster
+      overload; // wrnhhhhh
+    function Taste( d : TPuppyDog ) : TReaction;
+    function Taste( c : TKittyCat ) : TReaction;
+    function Taste( h : THamster )  : TReaction;
+    function TasteTest : boolean; override;
+  end;
+
+// the monster should love to eat puppy dogs
+function TGoalMonster.Taste( d : TPuppyDog ) : TReaction;
+  begin
+    writeln( d.name, ' taste good! yum!' );
+    result := kYUM;
+  end;
+  
+// it should hate to eat kitty cats
+function TGoalMonster.Taste( c : TKittyCat ) : TReaction;
+  begin
+    writeln( c.name, ' taste bad! yuck!' );
+    result := kYUCK;
+  end;
+  
+// it should never eat hamsters
+function TGoalMonster.Taste( h : THamster ) : TReaction;
+  begin
+    writeln( 'no eat ', h.name, '! ', h.name, ' my friend!' );
+    result := kNOEAT;
+  end;
+  
+// anything else should throw it into a rage:
+function TGoalMonster.Taste( p : TPet ) : TReaction;
+  begin
+    writeln('no want ', p.name, '! raaar!');
+    result := kRAAR;
+  end;
+
+// To summarize:
+function TGoalMonster.TasteTest : boolean;
+  begin
+    result := (Taste(dog) = kYUM)
+      and (Taste(cat) = kYUCK)
+      and (Taste(hamster) = kNOEAT)
+      and (Taste(frog) = kRAAR)
+  end;
+  
+{ Unfortunately, TGoalMonster didn't work as expected.
+  It was unable to distinguish the various types of
+  animal, so every potential meal simply enraged the
+  monster. It eventually starved to death, after nearly
+  destroying the entire lab.
+  
+ Thankfully, we were able to salvage enough of its
+ genetic material to create some working prototypes. }
+
+{--------------------------------------------------------------
+  Our first successful attempt involved adding a CASE
+  statement to the creature's brain.
+--------------------------------------------------------------}
+ 
+type
+  TCaseMonster = class( TGoalMonster )
+    function Taste( pet : TPet ) : TReaction; override;
+  end;
+
+function TCaseMonster.Taste( pet : TPet ) : TReaction;
   begin
     // case ( class of p ) of  <- nope :/
     { There doesn't seem to be any special syntax to
@@ -39,38 +127,99 @@ procedure monster.eat( p : pet );
       operator. Either way, if you want to dispatch
       to a type-specific function, you need to use the
       'as' operator to cast it. }
-    case p.classtype.classname of
-      'hamster'	: writeln('monster no eat hamster. hamster friend.');
-      else if p is dog then self.eat( p as dog )
-      else if p is cat then self.eat( p as cat )
-      else writeln( 'monster no want that! raaar!' );
+    case pet.ClassType.ClassName of
+      'TPuppyDog' : result := self.Taste( pet as TPuppyDog );
+      'TKittyCat' : result := self.Taste( pet as TKittyCat );
+      'THamster'  : result := self.Taste( pet as THamster );
+    else
+      result := inherited Taste(pet)
     end
   end;
-
-procedure monster.eat( d : dog );
+  
+  
+{--------------------------------------------------------------
+  Our next attempt involved using a swarm of helper viruses
+  to make each animal's flavor more distinctive.
+--------------------------------------------------------------}
+type
+  THelpMonster = class( TGoalMonster )
+    function Taste( pet : TPet ) : TReaction; override;
+  end;
+  TStrongerFlavor = class helper for TPet
+    function GetEatenBy( m : THelpMonster ) : TReaction; overload;
+  end;
+  TPuppyDogFlavor = class helper(TStrongerFlavor) for TPuppyDog
+    function GetEatenBy( m : THelpMonster ) : TReaction; overload;
+  end;
+  
+{ unfortunately, the animals would only adapt the 'rage' flavor: }
+function TStrongerFlavor.GetEatenBy( m : THelpMonster ) : TReaction;
   begin
-    writeln( d.name, ' taste good! yum!' );
+    writeln( name, ' enrages the monster.');
+    result := kRAAR
+  end;
+  
+{ Attempting to customize the virus for the individual animals proved
+  fruitless. We could have modified their brains using our earlier
+  CASE statement technology, but this would have just made THelpMonster
+  a more expensive version of TCaseMonster. }
+function TPuppyDogFlavor.GetEatenBy( m : THelpMonster ) : TReaction;
+  begin
+    result := kYUM
   end;
 
-procedure monster.eat( c : cat );
+function THelpMonster.Taste( pet : TPet ) : TReaction;
   begin
-    writeln( c.name, ' taste bad! yech!' );
+    result := pet.GetEatenBy(self);
   end;
+  
 
-var a, b, c, d : pet; m : monster;
+{-- test runner -----------------------------------------------}
+
+procedure Test(m:TMonster);
+  begin
+    writeln('-------------------------------');
+    writeln('testing ', m.ClassType.ClassName );
+    writeln('-------------------------------');
+    if m.TasteTest then
+      begin
+        writeln('result: PASSED!');
+        m.Free
+      end
+    else
+      begin
+        writeln('result: FAILED!');
+        m.Destroy // Tragic, yes, but it keeps our children safe.
+      end;
+    writeln;
+  end;
+  
 begin
-  a := dog.create( 'sparky' );
-  b := cat.create( 'felix' );
-  c := hamster.create( 'fluffy' );
-  d := pet.create( 'some other thing ');
-  m := monster.create;
-  // m.eat( a as a.ClassType );  // I wish. :/
-  m.eat( a ); m.eat( b ); m.eat( c ); m.eat( d );
+  PrepareBuffet;
+  Test(TGoalMonster.Create);
+  Test(TCaseMonster.Create);
+  Test(THelpMonster.Create);
 end.
 { output:
-  ------
-  sparky taste good! yum!
-  felix taste bad! yech!
-  monster no eat hamster. hamster friend.
-  monster no want that! raaar!
+  
+  -------------------------------
+  testing TGoalMonster
+  -------------------------------
+  no want Sparky! raaar!
+  result: FAILED!
+  
+  -------------------------------
+  testing TCaseMonster
+  -------------------------------
+  Sparky taste good! yum!
+  Quaxo taste bad! yuck!
+  no eat Goldie! Goldie my friend!
+  no want Mr Jumpy! raaar!
+  result: PASSED!
+
+  -------------------------------
+  testing THelpMonster
+  -------------------------------
+  Sparky enrages the monster.
+  result: FAILED!
 }
