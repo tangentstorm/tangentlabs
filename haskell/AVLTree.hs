6f73703966dedf8e13765bd6c3c543ecef62cd36
sabren <michal.wallace@gmail.com>
2013-12-12 03:11:33 -0600

    AVLTree.hs : start of a test suite, and an implementation of the balance check.

--- haskell/AVLTree.hs
+++ haskell/AVLTree.hs
@@ -1,12 +1,17 @@
 module Main where
+import Test.HUnit
+
 -----------------------------------------------------------------
 -- core tree datatype
 -----------------------------------------------------------------
 
+data Bal = Lh | B | Rh          -- left heavy, balanced, right heavy
+   deriving (Show, Eq)
+
 data Tree a
-   = E                              -- empty node
-   | L a                            -- leaf node
-   | T (Tree a) a (Tree a)          -- inner tree node
+   = E                          -- empty node
+   | L a                        -- leaf node
+   | T (Tree a) a (Tree a)      -- inner tree node
 
 instance (Show a) => Show (Tree a) where
   show (E)       = "_"
@@ -25,27 +30,42 @@ rotl (T p q (T r s t)) = (T (T p q r) s t)
 rotl other = other
 
 
-
 -- rotate right:  ((p q r) s t) --> (p q (r s t))
 rotr :: Tree a -> Tree a
 ----
 rotr (T (T p q r) s t) = (T p q (T r s t))
 rotr other               = other
 
+-----------------------------------------------------------------
+-- queries
+-----------------------------------------------------------------
+
+height :: Tree a -> Int
+------
+height (E)       = 0
+height (L _)     = 0
+height (T p q r) = 1 + max (height p) (height r)
+
+
+bal :: Tree a -> Bal
+----------
+bal (E)       = B
+bal (L _)     = B
+bal (T p _ q)
+    | hp ==  hq = B
+    | hp > hq+1 = Lh
+    | hp+1 < hq = Rh
+    | otherwise = B
+    where hp = (height p)
+          hq = (height q)
 
 -----------------------------------------------------------------
 -- tree building
 -----------------------------------------------------------------
 
 balance :: Tree a -> Tree a
-balance tree = tree
 -------
-
-cmp :: (Ord a) => a -> Tree a -> Ordering
----
-cmp v (E) = EQ
-cmp v (L x) = compare v x
-cmp v (T p q r) = compare v q
+balance other = other
 
 
 insert :: (Ord a) => Tree a -> a -> Tree a
@@ -73,6 +93,30 @@ fromList xs = foldl insert E xs
 -----------------------------------------------------------------
 
 
+-----------------------------------------------------------------
+-- test suite
+-----------------------------------------------------------------
+
+test_height = TestCase ( do
+     assertEqual "height (E)"  0 $ height E
+   )
+test_bal = TestCase ( do
+     assertEqual "bal (E)"     B $ bal E
+     assertEqual "bal (L _)"   B $ bal $ L 1
+     assertEqual "bal (E _ E)" B $ bal $ T E     2 (E)
+     assertEqual "bal (E _ L)" B $ bal $ T E     2 (L 3)
+     assertEqual "bal (L _ L)" B $ bal $ T (L 1) 2 (E)
+     assertEqual "bal (L _ L)" B $ bal $ T (L 1) 2 (L 3)
+
+     assertEqual "bal (h1 _ h0)" B  $ bal $ T (T E 1 E) 2 E
+     assertEqual "bal (h2 _ h0)" Lh $ bal $ T (T E 1 (T E 2 E)) 2 E
+     assertEqual "bal (h0 _ h2)" Rh $ bal $ T E 0 (T E 1 (T E 2 E))
+   )
+tests = TestList [
+    TestLabel "height"  test_height,
+    TestLabel "balance" test_bal
+  ]
+
 
 -----------------------------------------------------------------
 -- main routine
@@ -80,4 +124,7 @@ fromList xs = foldl insert E xs
 
 main :: IO ()
 ----
-main = let avl = fromList [8,6,7,5,3,0,9] in print avl
+main = do
+  counts <- runTestTT tests
+  print $ fromList [8,6,7,5,3,0,9]
+  return ()
