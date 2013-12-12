0668135909dd55f8f0e06d3e569d66105b114511
sabren <michal.wallace@gmail.com>
2013-12-12 01:52:51 -0600

    AVLTree.hs : reformatting

--- haskell/AVLTree.hs
+++ haskell/AVLTree.hs
@@ -20,6 +20,7 @@ instance (Show a) => Show (Tree a) where
 
 -- rotate left:  (p q (r s t))   -->  ((p q r) s t)
 rotl :: Tree a -> Tree a
+----
 rotl (T p q (T r s t)) = (T (T p q r) s t)
 rotl other = other
 
@@ -27,6 +28,7 @@ rotl other = other
 
 -- rotate right:  ((p q r) s t) --> (p q (r s t))
 rotr :: Tree a -> Tree a
+----
 rotr (T (T p q r) s t) = (T p q (T r s t))
 rotr other               = other
 
@@ -37,29 +39,45 @@ rotr other               = other
 
 balance :: Tree a -> Tree a
 balance tree = tree
+-------
+
+cmp :: (Ord a) => a -> Tree a -> Ordering
+---
+cmp v (E) = EQ
+cmp v (L x) = compare v x
+cmp v (T p q r) = compare v q
 
 
 insert :: (Ord a) => Tree a -> a -> Tree a
+------
+insert (E) y = L y   {- empty nodes just become new leaves -}
 
--- empty nodes are simply replaced with leaves
-insert (E) y = L y
-
-insert (L x)  y
-     | y  < x = balance $ T (L y) x E
+insert (L x)  y      {- leaves become branches -}
+     | y  < x = T (L y) x E
      | y == x = L x                     {- ignore duplicate entries -}
-     | y  > x = balance $ T E y (L x)
+     | y  > x = T E y (L x)
 
 insert (T p q r) y
      | y  < q  = balance $ T (insert p y) q r
      | y == q  = T p q r                {- again, ignore duplicates -}
      | y  > q  = balance $ T p q (insert r y)
 
+
 fromList :: (Ord a) => [a] -> Tree a
+--------
 fromList xs = foldl insert E xs
+
 
 -----------------------------------------------------------------
 -- lookup
 -----------------------------------------------------------------
 
+
+
+-----------------------------------------------------------------
+-- main routine
+-----------------------------------------------------------------
+
 main :: IO ()
+----
 main = let avl = fromList [8,6,7,5,3,0,9] in print avl
