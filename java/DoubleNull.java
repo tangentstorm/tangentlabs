1e9fbcf32cd68288ecb85cbea9b70c9b04229907
tangentstorm <michal.wallace@gmail.com>
2014-02-07 20:26:49 -0600

    java: a response to the question, "why can't a double be null?"

---
+++ java/DoubleNull.java
@@ -0,0 +1,10 @@
+class DoubleNull {
+    double x = null;
+}
+
+// DoubleNull.java:2: incompatible types
+//     found   : <nulltype>
+// 	required: double
+// 	    double x = null;
+//                ^
+// 1 error
