04447552ea60cbcd0f1f5edc709c9f0f71ddf981
tangentstorm <michal.wallace@gmail.com>
2014-05-24 23:26:01 -0500

    nial-mode: added builtin words

--- nial/nial-mode.el
+++ nial/nial-mode.el
@@ -5,10 +5,10 @@
 ; http://www.emacswiki.org/emacs/ModeTutorial
 
 ;;; Commentary:
-;; 
+;;
 
 ;;; History:
-;; 
+;;
 
 ;;; Code:
 
@@ -26,20 +26,72 @@
   (list
 
    ; keywords
-   (cons (make-optre '("if" "else" "elseif" "end" "endif" "then" "case" "from"))
+   (cons (make-optre
+	  '("gets" "endwhile" "endrepeat" "then" "repeat" "op" "local"
+	    "if" "else" "do" "case" "begin" "end" "endif" "then" "case"
+	    "from" ))
 	 font-lock-keyword-face)
-   
-
 
    ; predefined words
-   (cons (make-optre '(":=" "<" ">" "is" ))
-	 font-lock-function-name-face)
+   (cons (make-optre '(
+         "IS" "." "(" "!" "#" ")" "," "+" "*" "-" "<<" ";" ":="
+         ":" "/" "<" ">>" "<=" ">" "=" ">=" "@"
+	 "and" "abs" "allbools" "accumulate" "across" "allints"
+	 "allchars" "allin" "allreals" "allnumeric" "append"
+	 "arcsin" "arccos" "appendfile" "apply" "arctan" "atomic"
+	 "assign" "atversion" "axes" "cart" "bykey" "break" "blend"
+	 "breaklist" "breakin" "bye" "bycols" "callstack" "byrows"
+	 "choose" "char" "ceiling" "catenate" "charrep" "check_socket"
+	 "cos" "content" "close" "clearws" "clearprofile" "cols"
+	 "converse" "continue" "copyright" "cosh" "cull" "count"
+	 "diverse" "deepplace" "cutall" "cut" "display" "deparse"
+	 "deepupdate" "descan" "depth" "diagram" "div" "divide"
+	 "drop" "down" "eachboth" "eachall" "each" "dropright"
+	 "eachleft" "eachright" "edit" "endfor" "end" "elseif"
+	 "empty" "endcase" "endif" "for" "expression" "exit"
+	 "except" "erase" "equal" "eval" "eraserecord" "execute"
+	 "exp" "external" "exprs" "findall" "find" "false" "fault"
+	 "falsehood" "filestatus" "filelength" "filepath" "filetally"
+	 "filter" "floor" "first" "flip" "fold" "from" "fork" "fuse"
+	 "fromraw" "front" "gage" "getfile" "getdef" "getcommandline"
+	 "getenv" "getname" "hitch" "grid" "grade" "getsyms" "gradeup"
+	 "gt" "gte" "host" "in" "inverse" "innerproduct" "inner" "inv"
+	 "ip" "[" "ln" "link" "isboolean" "isinteger" "ischar" "isfault"
+         "isreal" "isphrase" "isstring" "iterate" "istruthvalue" "leaf"
+         "last" "laminate" "like" "libpath" "library" "list" "load"
+         "loaddefs" "nonlocal" "max" "match" "log" "lt" "lower" "lte"
+	 "mate" "min" "maxlength" "mod" "mix" "minus" "nialroot" "mold"
+	 "not" "numeric" "null" "no_op" "no_expr" "notin" "no_tr"
+	 "operation" "open" "or" "opposite" "opp" "ops" "plus" "pick"
+	 "pack" "outer" "pass" "pair" "parse" "partition" "paste"
+	 "phrase" "pi" "place" "picture" "placeall" "power" "positions"
+	 "post" "quotient" "putfile" "profile" "prod" "product"
+	 "profiletree" "profiletable" "quiet_fault" "raise" "reach"
+	 "random" "rank" "reciprocal" "read" "readfile" "readchar"
+	 "readarray" "readfield" "readscreen" "readrecord" "recip"
+	 "reduce" "recur" "reducerows" "reducecols" "reshape" "seek"
+	 "second" "rest" "reverse" "restart" "return_status" "scan"
+	 "save" "rows" "rotate" "seed" "see" "sublist" "sin" "simple"
+	 "shape" "setformat" "setdeftrace" "set" "seeusercalls"
+	 "seeprimcalls" "separator" "setwidth" "settrigger" "setmessages"
+	 "setlogname" "setinterrupts" "setprompt" "setprofile" "sinh"
+	 "single" "sqrt" "solitary" "sketch" "sleep" "socket_listen"
+	 "socket_accept" "socket_close" "socket_bind"
+	 "socket_connect" "socket_getline" "socket_receive"
+	 "socket_peek" "socket_read" "socket_send" "socket_write"
+	 "solve" "sort" "split" "sortup" "string" "status" "take"
+	 "symbols" "sum" "system" "tan" "tally" "takeright" "tanh"
+	 "tell" "team" "while" "until" "tr" "times" "third" "time"
+	 "timeit" "toupper" "tolower" "timestamp" "tonumber"
+	 "toraw" "toplevel" "transformer" "type" "transpose"
+	 "true" "trs" "twig" "truth" "unequal" "variable"
+	 "valence" "up" "updateall" "update" "vacate" "value"
+	 "version" "vars" "watchlist" "watch" "void" "with"
+	 "write" "writechars" "writearray" "writefile"
+	 "writefield" "writescreen" "writerecord" "]" "{" "}" "|" "~="
+	 )) font-lock-function-name-face)
 
-   ; predefined words
-   (cons (make-optre '("op" "tr" "ex"))
-	 font-lock-builtin-face))
-
-  "highlighting for nial mode")
+   ) "highlighting for nial mode")
 
 
 (defvar nial-indent 4)
