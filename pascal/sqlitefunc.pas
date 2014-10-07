9ba5e167ac2a1bd57e521b56fdbf4e49c32f9dc0
tangentstorm <michal.wallace@gmail.com>
2014-10-06 20:56:51 -0500

    sqlite custom function spike

--- pascal/sqlitefunc.pas
+++ pascal/sqlitefunc.pas
@@ -10,23 +10,58 @@
 program sqlitefunc;
 uses ctypes, sqlite3;
 
+// this gets called for every row in the result
 function callback(user: pointer; cols: cint; values, name: ppchar): cint; cdecl;
   begin
     writeln('! callback triggered');
-    writeln('  values:', values^);
+    writeln('  key: ', name^);
+    writeln('  val: ', values^);
     result := 0;
   end;
 
+procedure myXFunc(ctx: psqlite3_context; N: cint; V: ppsqlite3_value); cdecl;
+  var i : byte; val : pChar;
+  begin
+    writeln('! xFunc triggered');
+    writeln('  arg count: ', N);
+    if n > 0 then begin
+      writeln('  arg values: ');
+      for i := 0 to n-1 do begin
+	val := sqlite3_value_text(v[i]);
+	writeln('    ', i, ':', val);
+      end
+    end;
+    sqlite3_result_text(ctx, 'hello there!', -1, nil);
+  end;
+
+
 var
   dbName : PChar = ':memory:';
   dbh	 : PSQLite3;
   err	 : CInt;
-  msg	 : PChar;
+  msg,sql: PChar;
+  
 begin
-  writeLn('--creating in-memory database');
+  writeLn('--opening new in-memory database');
   sqlite3_open(dbName, @dbh);
-  err := sqlite3_exec(dbh, 'select 2+2 as iv', @callback, nil, @msg);
-  writeln('> error code: ', err);
+
+  // execute a query to make sure everything's set up ok.
+  sql := 'select 2+2 as iv union values(''four'')';
+  err := sqlite3_exec(dbh, sql, @callback, nil, @msg);
+  assert(err = 0);
+
+  // add the function
+  sqlite3_create_function(dbh, 'sayhello', 1, SQLITE_UTF8,
+			  {userData} nil,
+			  {xFunc}  @myXFunc,
+			  {xStep}  nil,
+			  {xFinal} nil);
+
+  // and now invoke it
+  sql := 'select sayhello(123)';
+  err := sqlite3_exec(dbh, sql, @callback, nil, @msg);
+  assert(err = 0);
+  
   sqlite3_close(dbh);
   writeLn('--all done.');
 end.
