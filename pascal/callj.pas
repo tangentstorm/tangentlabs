e097d74026a83727d17ca9bc7e4a240be579c856
tangentstorm <michal.wallace@gmail.com>
2014-12-01 18:20:56 -0600

    pascal/callj: cwio support

--- pascal/callj.pas
+++ pascal/callj.pas
@@ -1,5 +1,7 @@
 // prototype for calling j from pascal
-{$mode objfpc}
+// this program supports using cwio as a wrapper for
+// multi-colored text output and keyboard i/o
+{$mode delphiunicode}{$i xpc}
 program callj;
 uses xpc,dynlibs;
 
@@ -25,36 +27,42 @@ var
   JGetL : TJGetL;
   JGetA : TJGetA;
   JSetA : TJSetA;
+  jwaiting:boolean=false; // is j awaiting input?
 
-procedure DoWr(j:JJ;len:JI;s:JS); stdcall;
-  begin write(s)
+procedure DoWr(j : JJ;len:JI;s:JS); stdcall;
+  var i:byte;
+  begin
+    if (len > 0) and (s[0] in [^E, ^K]) then begin jwaiting:=true end;
+    write(s); flush(output);
   end;
 
 function DoRd(j:JJ; s:JS):JS; stdcall;
   var r:RawByteString;
-  begin write(s); readln(r); result:=JS(r);
+  begin
+    if jwaiting then jwaiting := false // j code already told cwio to expect input
+    else begin writeln(^E); flush(output); end;
+    readln(r); result:=JS(r);
   end;
-
+
 var jlib: TLibHandle; j:JJ; s:RawByteString='';
     jcb:TJCBs=(@DoWr, Nil, @DoRd, Nil, Pointer(3));
 begin
   jlib := LoadLibrary('./libj.so');
   if jlib = NilHandle then writeln('failed to load jlib')
-  else writeln('j loaded');
-
-  JInit := TJInit(GetProcedureAddress(jlib, 'JInit'));
-  JSM   := TJSM(GetProcedureAddress(jlib, 'JSM'));
-  JDo   := TJDo(GetProcedureAddress(jlib, 'JDo'));
-  JFree := TJFree(GetProcedureAddress(jlib, 'JFree'));
-  JGetL := TJGetL(GetProcedureAddress(jlib, 'JGetLocale'));
-  JGetA := TJGetA(GetProcedureAddress(jlib, 'JGetA'));
-  JSetA := TJSetA(GetProcedureAddress(jlib, 'JSetA'));
-
-  j := JInit(); // careful! need () because it's a variable!
-  JSM(j, jcb);
-  repeat
-    if s<>'' then JDo(j, JS(s)) else ok;
-    write('   '); readln(s);
-  until s='bye';
-  JFree(j);
+  else begin
+    JInit := TJInit(GetProcedureAddress(jlib, 'JInit'));
+    JSM   := TJSM(GetProcedureAddress(jlib, 'JSM'));
+    JDo   := TJDo(GetProcedureAddress(jlib, 'JDo'));
+    JFree := TJFree(GetProcedureAddress(jlib, 'JFree'));
+    JGetL := TJGetL(GetProcedureAddress(jlib, 'JGetLocale'));
+    JGetA := TJGetA(GetProcedureAddress(jlib, 'JGetA'));
+    JSetA := TJSetA(GetProcedureAddress(jlib, 'JSetA'));
+    j := JInit(); // careful! need () because it's a variable!
+    JSM(j, jcb);
+    repeat
+      if s='' then ok else JDo(j, JS(s));
+      writeln('   ',^E); flush(output); readln(s);
+    until (s='bye') or ((length(s)=1) and (s[1]=^D));
+    JFree(j);
+  end;
 end.
