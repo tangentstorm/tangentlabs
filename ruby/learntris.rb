e38be2308d2d1574f147d84ed5d7840fbd16c510
tangentstorm <michal.wallace@gmail.com>
2015-05-17 19:46:49 -0500

    learntris.rb: implement p and q

--- ruby/learntris.rb
+++ ruby/learntris.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-STDIN.readline
+# coding: utf-8
+# --------------------------------------------------------------
+# a learntris implementation in ruby
+# copyright © 2015 michal j wallace <http://tangentstorm.com/>
+# https://github.com/learnprogramming/learntris
+# --------------------------------------------------------------
+
+loop do
+  STDIN.readline.each_char do |c|
+    case c
+    when 'q' then exit!
+    when 'p' then 22.times { 10.times { print '. ' }; puts'' }
+    end
+  end
+end
