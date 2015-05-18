c88a81728b0e432fdd36e694439a95b1a02cdea6
tangentstorm <michal.wallace@gmail.com>
2015-05-17 20:04:00 -0500

    learntris.rb: use a matrix

--- ruby/learntris.rb
+++ ruby/learntris.rb
@@ -5,12 +5,23 @@
 # copyright © 2015 michal j wallace <http://tangentstorm.com/>
 # https://github.com/learnprogramming/learntris
 # --------------------------------------------------------------
+require 'matrix'
+
+@m = Matrix.build(22, 10) {'.'}
+
+def print_matrix
+  @m.row_vectors.each do |row|
+    row.each {|ch| print "#{ch} "}
+    puts
+  end
+end
+
 
 loop do
   STDIN.readline.each_char do |c|
     case c
     when 'q' then exit
-    when 'p' then 22.times { 10.times { print '. ' }; puts'' }
+    when 'p' then print_matrix
     end
   end
 end
