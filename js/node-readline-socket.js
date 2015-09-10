622190fdef2642a6546ca03e11caba929c719e80
sabren <michal.wallace@gmail.com>
2015-09-10 18:48:53 -0500

    removed evil 'function' keywords

--- js/node-readline-socket.js
+++ js/node-readline-socket.js
@@ -2,14 +2,14 @@
 var net = require('net'),
     readline = require('readline')
 
-var server = net.createServer(function(client) {
+var server = net.createServer((client)=> {
     var rl = readline.createInterface(client, client)
-    rl.question("what up, homie?", function(whatup) {
+    rl.question("what up, homie?", (whatup)=> {
         console.log(whatup)
         client.end("good talk. seeya.\r\n")
     })
 })
 
-server.listen(1313, function () {
+server.listen(1313, ()=> {
     console.log('listening...')
 })
