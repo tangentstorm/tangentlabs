6d9366f2b9f36e88a59e3cc28befe05bba850263
sabren <michal.wallace@gmail.com>
2015-09-10 18:38:10 -0500

    node readline + sockets

---
+++ js/node-readline-socket.js
@@ -0,0 +1,15 @@
+"use strict"
+var net = require('net'),
+    readline = require('readline')
+
+var server = net.createServer(function(client) {
+    var rl = readline.createInterface(client, client)
+    rl.question("what up, homie?", function(whatup) {
+        console.log(whatup);
+        client.end("good talk. seeya.\r\n")
+    })
+})
+
+server.listen(1313, function () {
+    console.log('listening...')
+})
