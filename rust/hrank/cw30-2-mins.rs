b56a8b7648d038d70c1d36ad17b86bc51b98ac39
Michal Wallace <michal.wallace@gmail.com>
2017-03-14 22:07:55 -0400

    mins.rs: remove comments that were both snarky and wrong. :)

--- rust/hrank/cw30-2-mins.rs
+++ rust/hrank/cw30-2-mins.rs
@@ -3,14 +3,9 @@
 //
 // basically: generate an 'expression' to find the minimum of 2 ≤ n ≤ 50 ints.
 // example:       4 -> min(int, min(int, min(int, int)))
-// except i do:   4 -> min(min(int, int), min(int, int))
-// (i.e., i do n*log2(n) comparisons rather than n).
-//
-// ... except i don't really, because hackerrank only accepts the O(n) version.
 fn mins(i:u8)->String {
     if i==1 { format!("int") } // <shrug> this is what they ask for.
-    // else { let h0 = i>>1; let h1 = i-h0; format!("min({}, {})", mins(h0), mins(h1)) }
-    else { format!("min(int, {})", mins(i-1)) }     // dumb O(n) version
+    else { format!("min(int, {})", mins(i-1)) }
 }
 
 fn main(){
