8bbe168292190637d597a8a0472eaac623842a55
Michal Wallace <michal.wallace@gmail.com>
2018-02-08 19:25:22 -0500

    forthy: quit, swap, show commands

--- rust/forthy.rs
+++ rust/forthy.rs
@@ -3,23 +3,39 @@ use std::io;
 use std::io::Write;
 
 fn read()->String {
-    let mut buf = String::new();
-    print!("> ");
-    io::stdout().flush()                 .expect("couldn't flush stdout.");
-    io::stdin().read_line(&mut buf)      .expect("failed to read line.");
-    (buf)
+  let mut buf = String::new();
+  print!("> ");
+  io::stdout().flush()                 .expect("couldn't flush stdout.");
+  io::stdin().read_line(&mut buf)      .expect("failed to read line.");
+  (buf)
+}
+
+fn swap(data: &mut Vec<i32>) {
+  let p = data.len()-1;
+  if p > 1 {
+    let t = data[p-1];
+    data[p-1]=data[p];
+    data[p]=t;
+  }
+}
+
+fn show(data: &Vec<i32>) {
+  for x in data { print!("{} ", x); } println!("");
 }
 
 fn main() {
-    let mut data: Vec<i32> = Vec::new();
+  let mut data: Vec<i32> = Vec::new();
+  'main: loop {
     let line = read();
     // for ch in line.chars() { println!("{}", ch); }
     for word in line.split_whitespace() {
-        match word {
-            _     => if let Ok(w)=i32::from_str_radix(word, 10) { data.push(w); }
-                     else { println!("[{}]", word) },
-        }
+      match word {
+        "?" => show(&data),
+        "q" => break 'main,
+        "swap" => swap(&mut data),
+        _ => if let Ok(w)=i32::from_str_radix(word, 10) { data.push(w); }
+             else { println!("[{}]", word) },
+      }
     }
-    println!("done with execution. stack contents:");
-    for x in data { print!("{} ", x); } println!("");
+  }
 }
