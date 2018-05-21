0fc52e31391cce00df9aa553374efc67949e0ccf
tangentstorm <michal.wallace@gmail.com>
2018-05-21 16:06:24 -0400

    rust/forthy: whitespace

--- rust/forthy.rs
+++ rust/forthy.rs
@@ -7,35 +7,26 @@ fn read()->String {
   print!("> ");
   io::stdout().flush()                 .expect("couldn't flush stdout.");
   io::stdin().read_line(&mut buf)      .expect("failed to read line.");
-  (buf)
-}
+  buf}
 
 fn swap(data: &mut Vec<i32>) {
   let p = data.len()-1;
   if p > 1 {
     let t = data[p-1];
     data[p-1]=data[p];
-    data[p]=t;
-  }
-}
+    data[p]=t;  }}
 
 fn show(data: &Vec<i32>) {
-  for x in data { print!("{} ", x); } println!("");
-}
+  for x in data { print!("{} ", x); } println!("");}
 
 fn main() {
   let mut data: Vec<i32> = Vec::new();
   'main: loop {
     let line = read();
-    // for ch in line.chars() { println!("{}", ch); }
     for word in line.split_whitespace() {
       match word {
         "?" => show(&data),
         "q" => break 'main,
         "swap" => swap(&mut data),
         _ => if let Ok(w)=i32::from_str_radix(word, 10) { data.push(w); }
-             else { println!("[{}]", word) },
-      }
-    }
-  }
-}
+             else { println!("[{}]", word) }}}}}
