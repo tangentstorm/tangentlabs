9612d3e99a1ad2c9a925c3246cefe4be0aa55149
michal wallace <michal.wallace@gmail.com>
2018-07-04 03:47:22 -0400

    topspace.thy: proved the first theorem.

--- isar/TopSpace.thy
+++ isar/TopSpace.thy
@@ -20,45 +20,63 @@ class topspace =
       of A if the intersection of every neighborhood of x with A is non-empty.\<close>
 
     definition limpt :: "'a set \<Rightarrow> 'a \<Rightarrow> bool"
-      where "limpt A p \<longleftrightarrow> (A\<subset>X) \<and> (p\<in>X) \<and> ((A \<inter> \<Inter>T) \<noteq> {})"
+      where "limpt A p \<equiv> (A\<subset>X) \<and> (p\<in>X) \<and> ((A \<inter> \<Inter>T) \<noteq> {})"
 
     text \<open>If A\<subset>X in topological space (X,T), then x\<in>X is called an \<^bold>\<open>interior point\<close> 
       of A if at least one neighborhood of a is contained entirely within A.\<close>
 
     definition intpt :: "'a set \<Rightarrow> 'a \<Rightarrow> bool"
-      where "intpt A p \<longleftrightarrow> (A\<subset>X) \<and> (p\<in>A) \<and> (\<exists>N\<in>T. N\<subset>A)"
+      where "intpt A p \<equiv> (A\<subset>X) \<and> (p\<in>A) \<and> (\<exists>N\<in>T. N\<subset>A)"
 
     definition boundpt :: "'a set \<Rightarrow> 'a \<Rightarrow> bool"
-      where "boundpt A p \<longleftrightarrow> (limpt A p) \<and> (limpt (X-A) p)"
+      where "boundpt A p \<equiv> (limpt A p) \<and> (limpt (X-A) p)"
 
     definition "open" :: "'a set \<Rightarrow> bool"
-      where "open A = (\<forall>p\<in>A. (intpt A p))"
+      where "open A \<equiv> (\<forall>p\<in>A. (intpt A p))"
       (* where "open A \<longleftrightarrow> A = {a\<in>A. intpt A a}" *)
 
     definition closed :: "'a set \<Rightarrow> bool"
-      where "closed A \<longleftrightarrow> open (X-A)"
+      where "closed A \<equiv> open (X-A)"
 
-  theorem "closed A \<and> limpt A p \<longrightarrow> p \<in> A"
-    using Inf_lower Int_Diff closed_def intpt_def limpt_def open_def by auto
+  theorem assumes "closed A" and "limpt A p"  shows "p \<in> A"
+  proof (rule ccontr)
+    assume "p\<notin>A"
+    have "open (X-A)"
+      using `closed A` closed_def by simp
+    obtain N where "N\<in>T" and "p\<in>N" and "N\<subset>(X-A)"
+      using Inf_lower(* "x \<in> A \<Longrightarrow> \<Sqinter>A \<le> x" *)
+        and `open (X-A)` open_def intpt_def 
+        and `limpt A p` limpt_def by auto
+    hence "N\<inter>A = {}" by auto
+    moreover have "N\<inter>A \<noteq> {}"
+      using DiffD2  (*  "c \<in> A - B \<Longrightarrow> c \<in> B \<Longrightarrow> P" *)
+        and \<open>N\<in>T\<close> `limpt A p` limpt_def by auto
+    ultimately show "False" by simp
+  qed
 
-  (* theorem "closed A \<longleftrightarrow> {p\<in>X. limpt A p} \<subseteq> A" *)
-    (* proof - *)
-      (* fix p assume a0:"closed A" *)
-      (* show g:"limpt A p \<longrightarrow> p\<in>A" *)
-        (* proof - *)
-          (* assume p:"(limpt A p) \<and> (p\<notin>A)" *)
-          (* thus "False" sorry *)
-        
-          (* hence "p \<in> (X-A)"                using limpt_def by auto *)
-          (* have  "open (X-A)"               using a0 closed_def by auto *)
-          (* then obtain N where n0:"N\<in>T \<and> p\<in>N \<and> N\<subset>(X-A)" using DiffD2 intpt_def limpt_def open_def p by auto   *)
-          (* hence n1:"N\<inter>A ={}"                 by auto *)
-          (* also have n2:"N\<inter>A \<noteq> {}" using Inf_lower n0 calculation limpt_def p by auto *)
-          (* show g sledgehammer *)
 
-      (* thus "closed A \<Longrightarrow> {p\<in>X. limpt A p} \<subseteq> A" by auto *)
-    (* (*proof (rule ccontr) sorry*) *)
-    (* next show "{p\<in>X. limpt A p} \<subseteq> A \<Longrightarrow> closed A" sorry *)
-    (* qed *)
+
+
+
+
+
+   (* misc notes to self... *)
+
+    (* i had originally translated the theorem like so: *)
+    lemma (in topspace) "closed A \<Longrightarrow> {p\<in>X. limpt A p} \<subseteq>  A"
+      proof   oops
+  
+    (* simpler to just write this: *)
+    lemma (in topspace) "closed A \<and> limpt A p \<longrightarrow> p \<in> A"
+      proof -
+        show ?thesis using Inf_lower Int_Diff closed_def intpt_def limpt_def open_def by auto
+      qed
+  
+    (* but even simpler to write this. (compare subgoals in the output panel) *) 
+    lemma (in topspace) assumes "closed A" and "limpt A p"  shows "p \<in> A"
+      proof     oops
+  
+    (* but sentilles used a proof by contradiction, 
+       so in the end the goal to prove was just "False". *)
 
 end
