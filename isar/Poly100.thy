b00b7461c3f3e33d1ca80ef483b429b4cd96367a
michal wallace <michal.wallace@gmail.com>
2018-07-24 08:59:18 -0400

    Poly100: minor cleanups

--- isar/Poly100.thy
+++ isar/Poly100.thy
@@ -1,31 +1,31 @@
 (* Isar Proofs (eventually) for two of the theorems from http://www.cs.ru.nl/~freek/100/
 
    #13 Polyhedron Formula (F + V - E = 2)     (TODO)
-   #50 The Number of Platonic Solids 
+   #50 The Number of Platonic Solids
        (core logic is proven, except for a missing duplicate argument)
-   
+
 *)
 theory Poly100
   imports Main "HOL-Analysis.Polytope"
 begin
 
-(* ------------------------------------------------------------ *)
+(* ------------------------------------------------------------------------ *)
 section \<open>The Polyhedron Formula\<close>
-(* ------------- none of this is actually used yet ------------ *)
+(* ------------------------------------------------------------------------ *)
 
-definition edges 
-  where "edges p \<equiv> {e. e edge_of p}"
-definition vertices
-  where "vertices p \<equiv> {v. v face_of p \<and> aff_dim v = 0}"
-definition faces 
-  where "faces p \<equiv> {v. v face_of p \<and> aff_dim v = 2}"
+definition edges where
+  "edges p = {e. e edge_of p}"
+definition vertices where
+  "vertices p = {v. v face_of p \<and> aff_dim v = 0}"
+definition faces where
+  "faces p = {v. v face_of p \<and> aff_dim v = 2}"
 
-locale solid =
+locale convex_polyhedron =
   fixes p assumes "solid p" and "aff_dim p = 3"
 begin
 
 definition F where "F = card(faces p)"
-definition V where "V = card(vertices p)" 
+definition V where "V = card(vertices p)"
 definition E where "E = card(edges p)"
 
 theorem polyhedron_formula:
@@ -39,7 +39,6 @@ qed
 end
 
 
-
 (* ------------------------------------------------------------------------ *)
 section \<open>The Platonic Solids\<close>
 (* ------------------------------------------------------------------------ *)
@@ -47,7 +46,7 @@ section \<open>The Platonic Solids\<close>
 text \<open>There are exactly five platonic solids. The following theorem enumerates
       them by their Schläfli pairs \<open>(s,m)\<close>, using the polyhedron formula above.\<close>
 
-theorem (in solid) PLATONIC_SOLIDS:
+theorem (in convex_polyhedron) PLATONIC_SOLIDS:
   fixes s::nat \<comment> \<open>number of sides on each face\<close>
     and m::nat \<comment> \<open>number of faces that meet at each vertex\<close>
   assumes "s \<ge> 3"  \<comment> \<open>faces must have at least 3 sides\<close>
@@ -55,11 +54,11 @@ theorem (in solid) PLATONIC_SOLIDS:
       and "E>0"
       and eq0: "s * F = 2 * E"
       and eq1: "m * V = 2 * E"
-    shows "(s,m) \<in> {(3,3), (3,4), (3,5), (4,3), (5,3)}" 
+    shows "(s,m) \<in> {(3,3), (3,4), (3,5), (4,3), (5,3)}"
 proof -
   \<comment> \<open>Following Euler, as explained here:
       https://www.mathsisfun.com/geometry/platonic-solids-why-five.html\<close>
-  have iq0: "1/s + 1/m > 1/2" 
+  have iq0: "1/s + 1/m > 1/2"
   proof -
     have f:"F = 2*E/s" using `s \<ge> 3` eq0 `E>0`
       by (metis mult_eq_0_iff neq0_conv nonzero_mult_div_cancel_left of_nat_eq_0_iff of_nat_mult zero_neq_numeral)
@@ -83,13 +82,11 @@ proof -
       hence "1/5 + 1/m > 1/2" using iq0 by linarith
       hence "1/m > 3/10" by simp
       hence "m < 10/3" using less_imp_inverse_less by fastforce
-      hence "m < 10/3" by simp
       hence "m \<le> 3" by simp
       moreover have "m\<noteq>3" proof
         assume "m=3"
         with iq0 have "1/s + 1/3 > 1/2" by auto
         hence "1/s > 1/2 - 1/3" by simp
-        hence "1/s > 3/6 - 2/6" by simp
         hence "1/s > 1/6" by simp
         hence "s < 6" using div_by_1 floor_of_nat linorder_not_le by fastforce
         with `s > 5` show False by simp
@@ -98,18 +95,17 @@ proof -
       hence "False" using `m\<ge>3` by auto
       thus "\<not>s\<le>5 \<Longrightarrow> False" by simp
     qed
-  next 
+  next
     show "m\<le>5" \<comment> \<open>!HELP!: same argument as above, swapping m and s\<close>  sorry
   qed
   hence "s\<le>5 \<and> m\<le>5" using assms by auto
   hence "s \<in> {3,4,5}" and "m \<in> {3,4,5}" using assms by auto
-  hence "(s,m) \<in> {3,4,5} \<times> {3,4,5}" (is "(s,m) \<in> ?X") by auto
-  hence "(s,m) \<in> ?X" by simp
+  hence "(s,m) \<in> {3,4,5} \<times> {3,4,5}" by auto
   moreover have "(s,m) \<notin> {(4,4), (5,4), (4,5), (5,5)}" proof -
     have sm: "1/s + 1/m > 1/2" using iq0 by blast
     \<comment> \<open>!HELP! all of these are essentially the same. How can I simplify this?\<close>
     have 44: "(s,m) \<noteq> (4,4)" proof
-      assume a44: "(s,m) = (4,4)" hence "1/s+1/m\<ge>1/2" using sm by simp 
+      assume a44: "(s,m) = (4,4)" hence "1/s+1/m\<ge>1/2" using sm by simp
       thus False using a44 iq0 by auto qed
     have 54: "(s,m) \<noteq> (5,4)" proof
       assume a54: "(s,m) = (5,4)" hence "1/s+1/m\<ge>1/2" using sm by simp
