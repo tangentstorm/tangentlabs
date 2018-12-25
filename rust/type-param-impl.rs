2e3130cc45dd90443d029b04ba980ae4d36be416
tangentstorm <michal.wallace@gmail.com>
2018-12-24 21:33:22 -0500

    type-param-impl.rs: got it *fairly* close to what i want...

--- rust/type-param-impl.rs
+++ rust/type-param-impl.rs
@@ -1,17 +1,32 @@
 /// this is a prototype of how to arrange the types in bex::bdd.
-
+///
+/// the basic idea is that a Base has a Worker, and a Worker has a State,
+/// State and Worker are both traits, and I want to be able to construct
+/// a Base with a single call to Base::new(), possibly parameterized by
+/// the State and Worker types.
+///
+/// This implementation is very close to what I want.
+///
+/// It is still a tiny bit unpleasant, though, because I have to say
+/// 'Base<S, W<S>>' instead of just 'Base<S, W>' or 'Base<W<S>>'
+///
+use std::marker::PhantomData;
+
 // the end goal: Base should have a Worker. (think database, not "base class")
 
-struct Base<W:WorkerT> { worker:W }
+struct Base<S:StateT, W:WorkerT<S>>{
+  phantom: PhantomData<S>,
+  worker:W }
 
-impl<W:WorkerT> Base<W> {
-  fn new(worker:W)->Self { Base{ worker } }
+impl<S:StateT,W:WorkerT<S>> Base<S,W> {
+  fn new(x:u32)->Self { Base{ worker:W::new(x), phantom: PhantomData } }
   fn val(&self)->u32 { self.worker.val() }}
 
-
+
 // WorkerT is just a trait
 
-trait WorkerT {
+trait WorkerT<S:StateT> : Sized {
+  fn new(x:u32)->Self;
   fn val(&self)->u32; }
 
 
@@ -19,27 +34,38 @@ trait WorkerT {
 
 struct Worker<S:StateT> { state:S }
 
-impl<S:StateT> Worker<S> {
-  fn new(state:S)->Self { Worker{ state } }}
-
-impl<S:StateT> WorkerT for Worker<S> {
+impl<S:StateT> WorkerT<S> for Worker<S> {
+  fn new(x:u32)->Self { Worker{ state: S::new(x) } }
   fn val(&self)->u32 { return self.state.val() }}
 
-
+
 // StateT is just another trait.
 
-trait StateT { fn val(&self)->u32; }
+trait StateT : Sized {
+  fn new(val:u32)->Self;
+  fn val(&self)->u32; }
 
 // State is an implementation of StateT:
 
 struct State { val: u32 }
-impl State {
-  fn new(val:u32)->Self { State { val }} }
-
 impl StateT for State {
+  fn new(val:u32)->Self { State { val }}
   fn val(&self)->u32 { return self.val } }
 
+
+#[test]
+fn test_main() {
+  let a = Base::<State, Worker<State>>::new(123);
+  let b = Base::<State, Worker<State>>::new(256);
+  assert_eq!(123, a.val());
+  assert_eq!(256, b.val()); }
 
-fn main() {
-  let b = Base::new(Worker::new(State::new(123)));
-  assert_eq!(123, b.val()); }
+// default base type
+type DefaultBase = Base<State, Worker<State>>;
+
+#[test]
+fn test_default() {
+  let a = DefaultBase::new(123);
+  let b = DefaultBase::new(256);
+  assert_eq!(123, a.val());
+  assert_eq!(256, b.val()); }
