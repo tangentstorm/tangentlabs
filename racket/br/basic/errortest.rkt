46811028a7cc1577fab09e105f9862f063d56bc0
tangentstorm <michal.wallace@gmail.com>
2022-04-02 20:44:35 -0400

    br/basic: ast simplifications in parser

--- racket/br/basic/errortest.rkt
+++ racket/br/basic/errortest.rkt
@@ -1,3 +1,12 @@
 #lang basic/parse-only
 10 print "hello world"
-25 printgoto
+25 goto 9 + 10 + 11
+30 end
+30 rem print 'ignored'
+35
+50 print "never gets here"
+40 end
+60 print 'three' : print 1.0 + 3
+70 goto 11. + 18.5 + .5 rem ignored
+10 print "o" ; "n" ; "e"
+20 print : goto 60.0 : end
--- racket/br/basic/parser.rkt
+++ racket/br/basic/parser.rkt
@@ -1,13 +1,13 @@
 #lang brag
-b-program : [b-line] (NL [b-line])*
-b-line : b-line-num [b-statement] (":" b-statement)* [b-rem]
-b-line-num : INTEGER
-b-statement : b-end | b-print | b-goto
+b-program : [b-line] (/NL [b-line])*
+b-line : b-line-num [b-statement] (/":" b-statement)* [b-rem]
+@b-line-num : INTEGER
+@b-statement : b-end | b-print | b-goto
 b-rem : REM
-b-end : "end"
-b-print : "print" [b-printable] (";" [b-printable])*
-b-printable : STRING | b-expr
-b-goto : "goto" b-expr
+b-end : /"end"
+b-print : /"print" [b-printable] (/";" [b-printable])*
+@b-printable : STRING | b-expr
+b-goto : /"goto" b-expr
 b-expr: b-sum
-b-sum : b-number ("+" b-number)*
-b-number : INTEGER | DECIMAL
+b-sum : b-number (/"+" b-number)*
+@b-number : INTEGER | DECIMAL
